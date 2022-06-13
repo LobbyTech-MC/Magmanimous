93aa8034b892c7e45e9a7d0cd4e128d43902fe07
haiman <83174104+haiman233@users.noreply.github.com>
2022-06-13 23:00:11 +0800

    汉化

--- src/main/java/io/ncbpfluffybear/magmanimous/items/MagmaItems.java
+++ src/main/java/io/ncbpfluffybear/magmanimous/items/MagmaItems.java
@@ -20,34 +20,34 @@ public class MagmaItems {
     public static final SlimefunItemStack MAGMANIMOUS_GUIDE = new SlimefunItemStack(
             "MAGMANIMOUS_GUIDE",
             Material.YELLOW_STAINED_GLASS_PANE,
-            "&6Magmanimous Guide",
-            "&e> Click here to get a link to the guide"
+            "&6熔岩之息指南书",
+            "&e> 单击此处获取指南的链接"
     );
 
     public static final SlimefunItemStack NETHER_CATALYST = new SlimefunItemStack(
             "NETHER_CATALYST",
             Material.MAGMA_CREAM,
-            "&cNether Catalyst",
-            "&7Used to start &4Nether Forge &7reactions"
+            "&c地狱催化剂",
+            "&7用于提升 &4地狱锻造厂 &7的工作效率"
     );
 
     // Nether forge components
     public static final SlimefunItemStack NETHER_FORGE_PEDESTAL = new SlimefunItemStack(
             "NETHER_FORGE_PEDESTAL",
             Material.DISPENSER,
-            "&cNether Forge Pedestal",
-            "&7Holds an item in the &4Nether Forge",
-            "",
+            "&c地狱锻造厂基座",
+            "&7持有一个物品",
+            "&7并放入 &4地狱锻造厂 &7里",
             Strings.NETHER_FORGE_COMPONENT
     );
 
     public static final SlimefunItemStack MAGMA_TANK = new SlimefunItemStack(
             "MAGMA_TANK",
             Material.MAGMA_BLOCK,
-            "&6Magma Tank",
-            "&7Stores up to " + MagmaTank.getMaxStorage() + " Magma Blocks",
-            "&e> Right Click &7to insert 1 Magma Block",
-            "&e> Sneak + Right Click &7to insert all held Magma Blocks",
+            "&6岩浆罐",
+            "&7最多储存" + MagmaTank.getMaxStorage() + "岩浆块",
+            "&e> 右击 &7存入一个岩浆块",
+            "&e> Shift+右击 &7存入所有手持岩浆块",
             "",
             Strings.NETHER_FORGE_COMPONENT
     );
@@ -56,9 +56,9 @@ public class MagmaItems {
     public static final SlimefunItemStack MAGMA_ATTUNED_HELMET = new SlimefunItemStack(
             "MAGMA_ATTUNED_HELMET",
             Material.NETHERITE_HELMET,
-            "&4Magma Attuned Helmet",
-            "&7Fire grants positive effects",
-            "&7if all four armor pieces are worn",
+            "&4岩浆调谐头盔",
+            "&7当穿上全套盔甲时",
+            "&7火焰将给予你增益效果",
             "",
             Strings.ATTUNEMENT
     );
@@ -66,9 +66,9 @@ public class MagmaItems {
     public static final SlimefunItemStack MAGMA_ATTUNED_CHESTPLATE = new SlimefunItemStack(
             "MAGMA_ATTUNED_CHESTPLATE",
             Material.NETHERITE_CHESTPLATE,
-            "&4Magma Attuned Chestplate",
-            "&7Fire grants positive effects",
-            "&7if all four armor pieces are worn",
+            "&4岩浆调谐胸甲",
+            "&7当穿上全套盔甲时",
+            "&7火焰将给予你增益效果",
             "",
             Strings.ATTUNEMENT
     );
@@ -76,9 +76,9 @@ public class MagmaItems {
     public static final SlimefunItemStack MAGMA_ATTUNED_LEGGINGS = new SlimefunItemStack(
             "MAGMA_ATTUNED_LEGGINGS",
             Material.NETHERITE_LEGGINGS,
-            "&4Magma Attuned Leggings",
-            "&7Fire grants positive effects",
-            "&7if all four armor pieces are worn",
+            "&4岩浆调谐护腿",
+            "&7当穿上全套盔甲时",
+            "&7火焰将给予你增益效果",
             "",
             Strings.ATTUNEMENT
     );
@@ -86,9 +86,9 @@ public class MagmaItems {
     public static final SlimefunItemStack MAGMA_ATTUNED_BOOTS = new SlimefunItemStack(
             "MAGMA_ATTUNED_BOOTS",
             Material.NETHERITE_BOOTS,
-            "&4Magma Attuned Boots",
-            "&7Fire grants positive effects",
-            "&7if all four armor pieces are worn",
+            "&4岩浆调谐靴子",
+            "&7当穿上全套盔甲时",
+            "&7火焰将给予你增益效果",
             "",
             Strings.ATTUNEMENT
     );
@@ -96,22 +96,22 @@ public class MagmaItems {
     public static final SlimefunItemStack PORTAL_STAR = new SlimefunItemStack(
             "PORTAL_STAR",
             Material.NETHER_STAR,
-            "&5Portal Star",
-            "&7Teleports a player to a linked",
-            "&7location when they enter a nether portal",
+            "&5传送门之星",
+            "&7当玩家进入地狱门时",
+            "&7传送他们至链接的位置",
             "",
-            "&e> Sneak + Right Click &7to link current location",
+            "&e> Shift+右击 &7链接地点",
             "",
-            "&cNo location currently bound",
+            "&c当前没有绑定位置",
             Strings.GADGET
     );
 
     public static final SlimefunItemStack COMBUSTION_POTION = new SlimefunItemStack(
             "COMBUSTION_POTION",
             Material.POTION,
-            "&6Combustion Potion",
-            "&7Sets drinker on fire for a minute",
-            "&7Useful for triggering attunement effects",
+            "&6燃烧药剂",
+            "&7点燃饮用者一分钟",
+            "&7用于触发岩浆调谐效果",
             "",
             Strings.CONSUMABLE
     );
