1cdbc88da695befefaa187d71a49d08d88de2f79
LobbyTech <1187755698@qq.com>
2024-01-07 23:46:48 +0800

    1

--- src/main/java/io/ncbpfluffybear/magmanimous/Magmanimous.java
+++ src/main/java/io/ncbpfluffybear/magmanimous/Magmanimous.java
@@ -1,19 +1,19 @@
 package io.ncbpfluffybear.magmanimous;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.ncbpfluffybear.magmanimous.listeners.AttunedListener;
 import io.ncbpfluffybear.magmanimous.listeners.InventoryListener;
 import io.ncbpfluffybear.magmanimous.listeners.ItemListener;
-import io.ncbpfluffybear.magmanimous.listeners.PiglinListener;
 import io.ncbpfluffybear.magmanimous.listeners.MagmanimousListener;
+import io.ncbpfluffybear.magmanimous.listeners.PiglinListener;
 import io.ncbpfluffybear.magmanimous.utils.MagmaItemSetup;
 import io.ncbpfluffybear.magmanimous.utils.ResearchSetup;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import net.guizhanss.guizhanlib.updater.GuizhanBuildsUpdater;
-import org.bstats.bukkit.Metrics;
-import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Main class of Magmanimous
@@ -43,9 +43,11 @@ public class Magmanimous extends JavaPlugin implements SlimefunAddon {
         // Register Research
         ResearchSetup.setup();
 
+        /*
         if (getConfig().getBoolean("options.auto-update") && getDescription().getVersion().startsWith("Build")) {
             new GuizhanBuildsUpdater(this, getFile(), "SlimefunGuguProject", "Magmanimous", "main", false, "zh-CN").start();
         }
+        */
     }
 
     @Nonnull
